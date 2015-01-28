cd7c9ae0c5464a36381f9944d3ddcaba926fa666
unknown <f.moutawe_admin@LTFRD7912045.emea.adecco.net>
2015-01-28 10:55:17 +0100

    Move error to a twig template

--- src/Application/Controller/Pages.php
+++ src/Application/Controller/Pages.php
@@ -7,6 +7,6 @@ use Silex\Application;
 class Pages {
 
   public function indexAction(Application $app) {
-    return $app['twig']->render('index.twig');
+    return $app['twig']->render('index.html.twig');
   }
 }
--- web/index.php
+++ web/index.php
@@ -9,8 +9,19 @@ $app
 	->register($ControllerServiceProvider)
 	->mount('/', $ControllerServiceProvider);
 $app
-	->error(function(\Exception $e, $code){
-		return new Response('We are sorry, but something went terribly wrong.');
+	->error(function(\Exception $e, $code) use ($app){
+		$message = '';
+
+		switch ($code) {
+			case 404:
+				$message .= 'The requested page is not found.';
+				break;
+			case 501:
+				$message .= 'The server encountered a temporary error and could not complete your request.';
+				break;
+		}
+
+		return $app['twig']->render('error.html.twig', ['code' => $code, 'message' => $message]);
 	});
 
 $app->run();
